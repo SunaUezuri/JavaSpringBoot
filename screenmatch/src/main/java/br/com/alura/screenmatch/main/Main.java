390ba7e1c9862e2909341bf806b7c5ece0aeeefd
SunaUezuri <wesley.jane.santos@gmail.com>
2024-12-14 00:02:01 -0300

    Criando dados estatísticos para os episódios e temporadas.

--- screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -132,5 +132,14 @@ public class Main {
                         Collectors.averagingDouble(Episodio::getAvaliacao)));
 
         System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade de episódios avaliados: " + est.getCount());
     }
 }
