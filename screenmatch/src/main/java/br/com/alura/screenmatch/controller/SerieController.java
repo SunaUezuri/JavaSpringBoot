643d6c21eb5c0baea8ab91358be173a795764bd6
SunaUezuri <wesley.jane.santos@gmail.com>
2025-02-15 11:20:08 -0300

    Modificando o método para exibir o dados do banco(contém um erro que exibe apenas uma série).

--- screenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,16 +1,24 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //Falando para a aplicação que esta classe é um controller
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
     //Anotação para o método GET
     @GetMapping("/series")
-    public String obterSeries(){
-        return "Hello World!";
+    public List<Serie> obterSeries(){
+        return repositorio.findAll();
     }
 
 }
